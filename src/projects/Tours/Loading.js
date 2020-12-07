d3e8863427fffd0ef23cacecd89f843a708b0a57
ssr <sheetalsinghnew@gmail.com>
2020-12-07 20:35:24 +0530

    Tour project

---
+++ src/projects/Tours/Loading.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import './index.css'
+export default function Loading() {
+    return (
+        <div className="loading">
+            <h1>loading</h1>
+        </div>
+    )
+}
