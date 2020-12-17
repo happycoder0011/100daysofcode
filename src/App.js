542dd98a554a2bc7baa102f7efdae818b8c40a01
ssr <sheetalsinghnew@gmail.com>
2020-12-17 21:07:52 +0530

    glitch

--- src/App.js
+++ src/App.js
@@ -3,13 +3,19 @@ import Homepage from './components/homepage';
 import Mood from './projects/Mood_Calender';
 import Timeline from './projects/Timeline/Timeline';
 import Birthday from './projects/Birthday_reminder';
+import Tour_Project from './projects/Tours';
+import Texteffect from './projects/Texteffects/Index';
 function App() {
   return (
     <div className="App">
+    <Texteffect/>
+    {/*
     <Birthday/>
     <Homepage/>
     <Timeline/>
     <Mood/>
+    <Tour_Project/>
+     */}
     </div>
   );
 }
---
+++ src/projects/Texteffects/Index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import './style.css';
+export default function Texteffect() {
+    return (
+        <div>
+            <h1 className="glitch"><span aria-hidden>100 DAYS OF CODE</span>100 DAYS OF CODE<span aria-hidden="true">100 DAYS OF CODE</span></h1>
+        </div>
+    )
+}
